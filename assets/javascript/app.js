6161b4e335a41fb0a40661a81e1506c1efcac005
Rebekah Mintac <rmintac@gmail.com>
2019-08-31 23:15:57 -0400

    .

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+var myQuestions = [
+    {
+        question: "Who breaks up on the Booze Cruise?",
+        answers: {
+            a: 'Pam and Roy',
+            b: 'Jim and Katy',
+            c: 'Kelly and Ryan'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "In season 2, how does Micharl injur his foot?",
+        answers: {
+            a: 'He gets his head stuck in a stair railing',
+            b: 'He crashes his car into a telephone pole',
+            c: 'He burns his foot on a George Foreman Grill'
+        },
+        correctAnswer: 'c'
+    }
+
+    }
+   
+
